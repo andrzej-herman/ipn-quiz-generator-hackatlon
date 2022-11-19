0be8f1061dbf651298dae2092ad4386699230274
Damian <niczymsaper@gmail.com>
2022-11-20 00:33:53 +0100

    Added first version of generating pdfs

--- QuizGeneratorApp/Program.cs
+++ QuizGeneratorApp/Program.cs
@@ -3,6 +3,7 @@ using QuizGeneratorApp.Database.BaseContext;
 using QuizGeneratorApp.Database.DbTools;
 using QuizGeneratorApp.Database.Repository;
 using QuizGeneratorApp.Services;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
@@ -20,7 +21,11 @@ builder.Services.AddDbContext<IpnQuizContext>(options =>
 
 builder.Services.AddScoped<IAuthRepository, AuthRepository>();
 builder.Services.AddScoped<IQuestionService, QuestionService>();
+builder.Services.AddScoped<IQuizGeneratorService, QuizGeneratorService>();
 var app = builder.Build();
+
+Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
